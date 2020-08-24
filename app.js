56a06a8095b9b02e1c6413654260d9c36bdbaa0e
avborik <avborik28@gmail.com>
2020-08-25 03:21:19 +1200

    + Updating score mehtods +Changing the active players methods

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var scores, roundScore, activePlayer, dice;
 
  scores = [0,0];
  roundScore = 0;
- activePlayer = 1;
+ activePlayer = 0;
  //document.querySelector('#current-'+ activePlayer).textContent = dice;
  //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
@@ -34,7 +34,23 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
     // 3. Update the round score If the rolled number was NOT a 1
+    if (dice !== 1){
+       roundScore += dice;
+       document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
 
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        //document.querySelector('.player-0-panel').classList.remove('active');
+        //document.querySelector('.player-1-panel').classList.add('active');
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        document.querySelector('.dice').style.display = 'none';
+    }
 });
 
 
