e7dce4875c354baa32161a1eae67804ed839f26c
avborik <avborik28@gmail.com>
2020-08-25 23:38:32 +1200

    + game init function

--- app.js
+++ app.js
@@ -10,22 +10,12 @@ GAME RULES:
 */
 var scores, roundScore, activePlayer, dice;
 
- scores = [0,0];
- roundScore = 0;
- activePlayer = 0;
+ init();
  //document.querySelector('#current-'+ activePlayer).textContent = dice;
  //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
 //var x = document.querySelector('#score-0').textContent;
 //console.log(x);
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-
 document.querySelector('.btn-roll').addEventListener('click',function(){
     // 1. Random number 
     dice = Math.floor(Math.random() * 6) + 1;
@@ -71,4 +61,26 @@ function nextPlayer(){
     document.querySelector('.player-1-panel').classList.toggle('active');
 
     document.querySelector('.dice').style.display = 'none';
-}
+};
+
+document.querySelector('.btn-new').addEventListener('click',init);
+
+function init(){
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+};
